5c0a9eec42bbc81b31c6e89424e6328b3624f667
d-dorazio <daniele@develer.com>
2017-10-28 19:08:10 +0200

    otto: add basic settings management system

---
+++ ports/esp32/dump_settings.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+from __future__ import print_function, unicode_literals
+
+import os
+
+
+# -----------------------------------------------------------
+# Settings Types
+# -----------------------------------------------------------
+
+def _str_setting(val):
+    return "'''{}'''".format(val)
+
+def _int_setting(val):
+    return str(int(val))
+
+
+# -----------------------------------------------------------
+# Settings
+# -----------------------------------------------------------
+
+WIFI_SETTINGS = (
+    ('WIFI_ESSID', _str_setting),
+    ('WIFI_PASSWORD', _str_setting),
+    ('WIFI_TIMEOUT', _int_setting),
+)
+
+
+# -----------------------------------------------------------
+# Main
+# -----------------------------------------------------------
+
+def main():
+    # in the future env could be a config file
+    env = os.environ
+    _dump_settings_group(env, 'Wifi', WIFI_SETTINGS)
+
+
+def _dump_settings_group(env, comment, settings):
+    print('#', comment)
+
+    for sett, ty in settings:
+        val = ty(env[sett])
+        print("{} = {}".format(sett, val))
+
+    print()
+
+
+if __name__ == '__main__':
+    main()
